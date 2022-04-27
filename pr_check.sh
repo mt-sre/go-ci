4a678c9c896c5c19d5ea6df9d346eaab22de3a95
Andrew Pantuso <apantuso@redhat.com>
2022-04-27 12:35:36 -0400

    ci: adding pre-commit and pr_check.sh

---
+++ pr_check.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -exvo pipefail -o nounset
+
+# utilize local go 1.17 version if available
+GO_1_17="/opt/go/1.17.7/bin"
+
+if [ -d  "${GO_1_17}" ]; then
+     PATH="${GO_1_17}:${PATH}"
+fi
+
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+DEPENDENCY_BIN="${PROJECT_ROOT}/.cache/dependencies/bin"
+PRECOMMIT="${DEPENDENCY_BIN}/pre-commit"
+
+function install_precommit() {
+     if [ -f "${PRECOMMIT}" ]; then
+          echo "pre-commit already installed"
+
+          return
+     fi
+
+     version=$1
+     url="https://github.com/pre-commit/pre-commit/releases/download/v${version}/pre-commit-${version}.pyz"
+
+     setup_dependencies && curl -sL "${url}" -o "${PRECOMMIT}" && chmod +x "${PRECOMMIT}"
+}
+
+function setup_dependencies() {
+     mkdir -p "${DEPENDENCY_BIN}"
+}
+
+function run-hooks() {
+     ${PRECOMMIT} run \
+     --from-ref origin/main \
+     --to-ref HEAD \
+     --show-diff-on-failure
+}
+
+install_precommit "2.17.0" && run-hooks && go test -v -cover -race -count=1 ./...
